a382b492f6649d66f0f2d2d9c305cadf671e4f84
LHS <gustjs1234596@gmail.com>
2024-09-22 18:15:03 +0900

    dotween

--- Assets/UpgradeManager.cs
+++ Assets/UpgradeManager.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour
 {
+    UpgradeManager instance;
+
     public int chairLevel;
     public int computerLevel;
     public int airconLevel;
@@ -13,19 +15,48 @@ public class UpgradeManager : MonoBehaviour
     private int airconFisrtConst;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-            
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //돈 획득 배율 관련
+    public int ChairMoneyGet()
     {
-        
+        //이거는 초 당
+        return (int)(chairFisrtConst + (chairFisrtConst * (chairFisrtConst * 0.5f)));
     }
 
-    public int ChairGetMoney()
+    public int AirconMoneyGet()
     {
-        return chairFisrtConst;
+        return (int)(airconFisrtConst);
+    }
+
+    public int ComputerMoneyGet()
+    {
+        return 0;
+    }
+
+    //코스트 소비 배율 관련
+    public int AirconConstGet()
+    {
+        return 0;
+    }
+
+    public int ComputerConstGet()
+    {
+        return 0;
+    }
+
+    public int ChairConstGet()
+    {
+        return 0;
     }
 }
